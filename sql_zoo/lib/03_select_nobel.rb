d07160310c661cfd6c0c9800a68f47ee1e4e0910
Daniel <danielwu116@gmail.com>
2021-03-23 15:00:59 -0400

    Finish 03_select_nobel

--- sql_zoo/lib/03_select_nobel.rb
+++ sql_zoo/lib/03_select_nobel.rb
@@ -62,6 +62,13 @@ end
 def millennial_peace_prizes
   # Give the name of the 'Peace' winners since the year 2000, including 2000.
   execute(<<-SQL)
+  SELECT
+    winner
+  FROM 
+    nobels
+  WHERE
+    yr >= 2000 AND subject = 'Peace';
+  
   SQL
 end
 
@@ -69,6 +76,12 @@ def eighties_literature
   # Show all details (yr, subject, winner) of the Literature prize winners
   # for 1980 to 1989 inclusive.
   execute(<<-SQL)
+  SELECT
+    yr, subject, winner
+  FROM
+    nobels
+  WHERE
+    subject = 'Literature' AND yr BETWEEN 1980 AND 1989;
   SQL
 end
 
@@ -76,11 +89,27 @@ def presidential_prizes
   # Show all details of the presidential winners: ('Theodore Roosevelt',
   # 'Woodrow Wilson', 'Jimmy Carter')
   execute(<<-SQL)
+  SELECT
+    *
+  FROM
+    nobels
+  WHERE
+    winner = 'Theodore Roosevelt' 
+      OR 
+    winner = 'Woodrow Wilson' 
+      OR 
+    winner = 'Jimmy Carter';
   SQL
 end
 
 def nobel_johns
   # Show the winners with first name John
   execute(<<-SQL)
+  SELECT
+    winner
+  FROM
+    nobels
+  WHERE
+    winner LIKE 'John%';
   SQL
 end
