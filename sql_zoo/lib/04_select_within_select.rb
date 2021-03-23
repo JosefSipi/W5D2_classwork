ac21615ed2b8a56522d9647a5064fcdec7ce6235
Joseph <Josephsipiorski@gmail.com>
2021-03-23 14:43:09 -0500

    made it through 04

--- sql_zoo/lib/04_select_within_select.rb
+++ sql_zoo/lib/04_select_within_select.rb
@@ -77,6 +77,26 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+  SELECT
+    name, continent
+  FROM
+    countries
+  WHERE
+    continent = (
+      SELECT
+        continent
+      FROM
+        countries
+      WHERE
+        name = 'Belize' 
+    ) OR continent = (
+      SELECT
+        continent
+      FROM
+        countries
+      WHERE
+        name = 'Belgium' 
+    );
   SQL
 end
 
@@ -84,6 +104,26 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+  SELECT
+    name, population
+  FROM
+    countries
+  WHERE
+    population > (
+    SELECT
+      population
+    FROM
+      countries
+    WHERE
+      name = 'Canada'
+  ) AND population < (
+    SELECT
+      population
+    FROM
+      countries
+    WHERE
+      name = 'Poland'
+  );
   SQL
 end
 
@@ -93,5 +133,18 @@ def sparse_continents
   # population.
   # Hint: Sometimes rewording the problem can help you see the solution.
   execute(<<-SQL)
+  SELECT
+    name, continent, population
+  FROM
+    countries
+  WHERE
+    population < 25000000 AND continent NOT IN (
+      SELECT
+        continent
+      FROM
+      countries
+      WHERE
+        population > 25000000
+    );
   SQL
 end
